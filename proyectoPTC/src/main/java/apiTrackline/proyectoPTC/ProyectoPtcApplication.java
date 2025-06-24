661bdb32f1f50091d75078f3d63b70953b138885
MarioIvan44 <20210050@ricaldone.edu.sv>
2025-06-24 13:50:52 -0600

    ARCHIVO PARA LA API

---
+++ proyectoPTC/src/main/java/apiTrackline/proyectoPTC/ProyectoPtcApplication.java
@@ -0,0 +1,13 @@
+package apiTrackline.proyectoPTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPtcApplication.class, args);
+	}
+
+}
