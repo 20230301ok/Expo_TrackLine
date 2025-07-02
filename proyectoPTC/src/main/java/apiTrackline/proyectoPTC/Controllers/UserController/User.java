02cdfeec02a36ddb1eb095fd520dd40122eea591
MarioIvan44 <20210050@ricaldone.edu.sv>
2025-07-02 11:10:14 -0500

    CRUD EN API TERMINADO

--- proyectoPTC/src/main/java/apiTrackline/proyectoPTC/Controllers/UserController/User.java
+++ proyectoPTC/src/main/java/apiTrackline/proyectoPTC/Controllers/UserController/User.java
@@ -3,9 +3,7 @@ package apiTrackline.proyectoPTC.Controllers.UserController;
 import apiTrackline.proyectoPTC.Models.DTO.DTOUser;
 import apiTrackline.proyectoPTC.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,29 @@ public class User {
     @GetMapping("/dataUser")
     public List<DTOUser> getUser(){
         return service.getData();
+    }
+
+    // Método para actualizar un usuario
+    //La ruta sería localhost:8080/apiUser/updateUser
+    @PutMapping("/updateUser/{id}")
+    public String updateUser(@PathVariable("id") long id, @RequestBody DTOUser userDto){
+        return service.update(id, userDto);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    //La ruta sería localhost:8080/apiUser/deleteUser
+    public String deleteUser(@PathVariable("id") long id) {
+        return service.delete(id);
+    }
+
+    @PostMapping("/postUser")
+    //La ruta sería localhost:8080/apiUser/postUser
+    public String postUser(@RequestBody DTOUser userDto) {
+        return service.post(userDto);
+    }
+
+    @PatchMapping("/updateUserPartial/{id}")
+    public String patchUser(@PathVariable Long id, @RequestBody DTOUser dto) {
+        return service.patchUser(id, dto);
     }
 }
--- proyectoPTC/src/main/java/apiTrackline/proyectoPTC/Services/UserService.java
+++ proyectoPTC/src/main/java/apiTrackline/proyectoPTC/Services/UserService.java
@@ -1,6 +1,5 @@
 package apiTrackline.proyectoPTC.Services;
 
-import apiTrackline.proyectoPTC.Controllers.UserController.User;
 import apiTrackline.proyectoPTC.Entities.UserEntity;
 import apiTrackline.proyectoPTC.Models.DTO.DTOUser;
 import apiTrackline.proyectoPTC.Repositories.UserRepository;
@@ -8,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,6 +32,65 @@ public class UserService {
         dto.setContrasenia(user.getContrasenia());
         dto.setIdRol(user.getIdRol());
         return dto;
+    }
+
+    public String update(long id, DTOUser userDto){
+        Optional<UserEntity> optionalUser = repo.findById(id);
+        if (optionalUser.isPresent()) {
+            UserEntity user = optionalUser.get();
+            user.setUsuario(userDto.getUsuario());
+            user.setContrasenia(userDto.getContrasenia());
+            user.setIdRol(userDto.getIdRol());
+            repo.save(user);
+            return "Usuario actualizado correctamente";
+        } else {
+            return "Usuario no encontrado con ID: " + (id);
+        }
+    }
+
+    public String delete(long id) {
+        Optional<UserEntity> optional = repo.findById(id);
+        if (optional.isPresent()) {
+            repo.deleteById(id);
+            return "Usuario eliminado correctamente";
+        } else {
+            return "Usuario no encontrado";
+        }
+    }
+
+    public String post(DTOUser dtoUser) {
+        try {
+            UserEntity user = new UserEntity();
+            user.setUsuario(dtoUser.getUsuario());
+            user.setContrasenia(dtoUser.getContrasenia());
+            user.setIdRol(dtoUser.getIdRol());
+
+            repo.save(user);
+            return "Usuario creado correctamente";
+        } catch (Exception e) {
+            return "Error al crear el usuario: " + e.getMessage();
+        }
+    }
+
+    public String patchUser(Long id, DTOUser dto) {
+        Optional<UserEntity> optional = repo.findById(id);
+        if (optional.isPresent()) {
+            UserEntity user = optional.get();
+
+            if (dto.getUsuario() != null) {
+                user.setUsuario(dto.getUsuario());
+            }
+            if (dto.getContrasenia() != null) {
+                user.setContrasenia(dto.getContrasenia());
+            }
+            if (dto.getIdRol() != 0) {
+                user.setIdRol(dto.getIdRol());
+            }
+
+            repo.save(user);
+            return "Usuario actualizado parcialmente";
+        }
+        return "Usuario no encontrado";
     }
 }
 
